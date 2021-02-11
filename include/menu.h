dd5e36e614850918f82a049e7ace3099a7146fd9
Daniel Hazelbaker <daniel@blueboxmoon.com>
2021-02-10 20:39:40 -0700

    - Updates to the menu interface.

--- include/menu.h
+++ include/menu.h
@@ -3,50 +3,150 @@
 
 #include <LiquidCrystal.h>
 
+/**
+ * The mode the dispenser motor should operate in.
+ */
 enum DispenserMode
 {
+    /**
+     * Each button press will move the motor a set number of
+     * steps.
+     */
     Step = 0,
-    Continuous = 1
+
+    /**
+     * While the button is pressed, the motor will move a set
+     * number of steps per second.
+     */
+    Drive = 1
 };
 #define DISPENSERMODE_LAST 1
 
+/**
+ * The direction the motor will move the plunger.
+ */
 enum DispenserDirection
 {
+    /**
+     * The plunger will move forward further into the dispenser.
+     */
     Forward = 0,
+
+    /**
+     * The plunger will back out of the dispenser.
+     */
     Backward = 1
 };
 #define DISPENSERDIRECTION_LAST 1
 
+/**
+ * Displays a menu for the user to use in configuring the device.
+ */
 class Menu
 {
 private:
+    /**
+     * The LCD display to use when interacting with the user.
+     */
     LiquidCrystal *m_lcd;
 
+    /**
+     * The current menu item being displayed.
+     */
     uint8_t m_currentMenu = 1;
 
+    /**
+     * Whether we are currently editing a value or not.
+     */
     bool m_isEditing = false;
 
+    /**
+     * Settings sub-menu mode currently being displayed.
+     */
+    uint8_t m_settingsMode = 0;
+
+    /**
+     * The current dispenser mode.
+     */
     DispenserMode m_mode = DispenserMode::Step;
 
+    /**
+     * The current direction of the dispenser.
+     */
     DispenserDirection m_direction = DispenserDirection::Forward;
 
-    int16_t m_stepSize = 0;
+    /**
+     * The number of steps to move on each button press in Step mode.
+     */
+    uint16_t m_stepSize = 0;
 
-    int16_t m_continuousSpeed = 0;
+    /**
+     * The number of steps to move per second while the button is
+     * depressed in Drive mode.
+     */
+    uint16_t m_driveSpeed = 0;
 
+    /**
+     * Reset the current values to sane defaults.
+     */
+    void resetValues();
+
+    /**
+     * Load the values from EEPROM.
+     */
+    void loadValues();
+
+    /**
+     * Save the current values into EEPROM.
+     */
+    void saveValues();
+
+    /**
+     * Update the display to reflect what should be in screen.
+     */
     void updateDisplay();
 
 public:
+    /**
+     * Creates a new instance of the Menu.
+     * 
+     * @param lcd The LCD object to display information on.
+     */
     Menu(LiquidCrystal *lcd);
 
+    /**
+     * Gets the current DispenserMode to operate in.
+     */
     DispenserMode getMode();
 
+    /**
+     * Gets the current direction to move the plunger.
+     */
     DispenserDirection getDirection();
 
+    /**
+     * Gets the number of steps to move in Step mode.
+     */
     uint16_t getStepSize();
 
+    /**
+     * Gets the number of steps per second to move in Drive mode.
+     */
+    uint16_t getDriveSpeed();
+
+    /**
+     * Signal that the UP key has been pressed.
+     */
     void keyUp();
+
+    /**
+     * Signal that the DOWN key has been pressed.
+     */
     void keyDown();
+
+    /**
+     * Signal that the SELECT key has been pressed.
+     */
     void keySelect();
 };
 
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,8 +1,16 @@
+#include <EEPROM.h>
 #include "menu.h"
 
 #define MAX(a, b) ((a) < (b) ? (b) : (a))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+const char *SettingsModes[4] = { "Cancel", "Load", "Save", "Init" };
+
+/**
+ * Creates a new instance of the Menu.
+ * 
+ * @param lcd The LCD object to display information on.
+ */
 Menu::Menu(LiquidCrystal *lcd)
 {
     m_lcd = lcd;
@@ -10,51 +18,83 @@ Menu::Menu(LiquidCrystal *lcd)
     m_lcd->blink();
     m_lcd->clear();
 
+    loadValues();
     updateDisplay();
 }
 
-DispenserMode Menu::getMode()
+/**
+ * Reset the current values to sane defaults.
+ */
+void Menu::resetValues()
 {
-    return m_mode;
+    m_mode = DispenserMode::Step;
+    m_direction = DispenserDirection::Forward;
+    m_stepSize = 5;
+    m_driveSpeed = 30;
 }
 
-DispenserDirection Menu::getDirection()
+/**
+ * Load the values from EEPROM.
+ */
+void Menu::loadValues()
 {
-    return m_direction;
+    uint8_t byteValue;
+
+    EEPROM.get<uint8_t>(0, byteValue);
+    m_mode = byteValue == DispenserMode::Step ? DispenserMode::Step : DispenserMode::Drive;
+    EEPROM.get<uint8_t>(1, byteValue);
+    m_direction = byteValue == DispenserDirection::Forward ? DispenserDirection::Forward : DispenserDirection::Backward;
+    EEPROM.get<uint16_t>(2, m_stepSize);
+    EEPROM.get<uint16_t>(4, m_driveSpeed);
 }
 
-uint16_t Menu::getStepSize()
+/**
+ * Save the current values into EEPROM.
+ */
+void Menu::saveValues()
 {
-    return m_stepSize;
+    EEPROM.put<uint8_t>(0, m_mode);
+    EEPROM.put<uint8_t>(1, m_direction);
+    EEPROM.put<uint16_t>(2, m_stepSize);
+    EEPROM.put<uint16_t>(4, m_driveSpeed);
 }
 
+/**
+ * Update the display to reflect what should be in screen.
+ */
 void Menu::updateDisplay()
 {
     m_lcd->clear();
 
     if (m_currentMenu == 1)
     {
-        m_lcd->print("1. Mode");
+        m_lcd->print("1. Settings");
         m_lcd->setCursor(0, 1);
-        m_lcd->print(m_mode == DispenserMode::Step ? "Step" : "Continuous");
+        m_lcd->print(SettingsModes[m_settingsMode]);
     }
     else if (m_currentMenu == 2)
     {
-        m_lcd->print("2. Direction");
+        m_lcd->print("2. Mode");
         m_lcd->setCursor(0, 1);
-        m_lcd->print(m_direction == DispenserDirection::Forward ? "Forward" : "Reverse");
+        m_lcd->print(m_mode == DispenserMode::Step ? "Step" : "Drive");
     }
     else if (m_currentMenu == 3)
     {
-        m_lcd->print("3. Step Size");
+        m_lcd->print("3. Direction");
         m_lcd->setCursor(0, 1);
-        m_lcd->print(m_stepSize);
+        m_lcd->print(m_direction == DispenserDirection::Forward ? "Forward" : "Reverse");
     }
     else if (m_currentMenu == 4)
     {
         m_lcd->print("4. Step Size");
         m_lcd->setCursor(0, 1);
         m_lcd->print(m_stepSize);
+    }
+    else if (m_currentMenu == 5)
+    {
+        m_lcd->print("5. Drive Speed");
+        m_lcd->setCursor(0, 1);
+        m_lcd->print(m_driveSpeed);
     }
 
     if (m_isEditing)
@@ -67,67 +107,147 @@ void Menu::updateDisplay()
     }
 }
 
+/**
+ * Gets the current DispenserMode to operate in.
+ */
+DispenserMode Menu::getMode()
+{
+    return m_mode;
+}
+
+/**
+ * Gets the current direction to move the plunger.
+ */
+DispenserDirection Menu::getDirection()
+{
+    return m_direction;
+}
+
+/**
+ * Gets the number of steps to move in Step mode.
+ */
+uint16_t Menu::getStepSize()
+{
+    return m_stepSize;
+}
+
+/**
+ * Gets the number of steps per second to move in Drive mode.
+ */
+uint16_t Menu::getDriveSpeed()
+{
+    return m_driveSpeed;
+}
+
+/**
+ * Signal that the UP key has been pressed.
+ */
 void Menu::keyUp()
 {
     if (m_isEditing)
     {
         if (m_currentMenu == 1)
         {
-            m_mode = (DispenserMode)MAX(0, m_mode - 1);
+            m_settingsMode = MIN(4, m_settingsMode + 1);
         }
         else if (m_currentMenu == 2)
         {
-            m_direction = (DispenserDirection)MAX(0, m_direction - 1);
+            m_mode = (DispenserMode)MIN(DISPENSERMODE_LAST, m_mode + 1);
         }
         else if (m_currentMenu == 3)
+        {
+            m_direction = (DispenserDirection)MIN(DISPENSERDIRECTION_LAST, m_direction + 1);
+        }
+        else if (m_currentMenu == 4)
         {
             m_stepSize = MIN(1000, m_stepSize + 1);
         }
-        else if (m_currentMenu == 3)
+        else if (m_currentMenu == 5)
         {
-            m_continuousSpeed = MIN(1000, m_continuousSpeed + 1);
+            m_driveSpeed = MIN(1000, m_driveSpeed + 1);
         }
     }
     else
     {
         m_currentMenu = MAX(1, m_currentMenu - 1);
+        m_settingsMode = 0;
     }
 
     updateDisplay();
 }
 
+/**
+ * Signal that the DOWN key has been pressed.
+ */
 void Menu::keyDown()
 {
     if (m_isEditing)
     {
         if (m_currentMenu == 1)
         {
-            m_mode = (DispenserMode)MIN(DISPENSERMODE_LAST, m_mode + 1);
+            m_settingsMode = MAX(0, m_settingsMode - 1);
         }
         else if (m_currentMenu == 2)
         {
-            m_direction = (DispenserDirection)MIN(DISPENSERDIRECTION_LAST, m_direction + 1);
+            m_mode = (DispenserMode)MAX(0, m_mode - 1);
         }
         else if (m_currentMenu == 3)
+        {
+            m_direction = (DispenserDirection)MAX(0, m_direction - 1);
+        }
+        else if (m_currentMenu == 4)
         {
             m_stepSize = MAX(0, m_stepSize - 1);
         }
-        else if (m_currentMenu == 3)
+        else if (m_currentMenu == 5)
         {
-            m_continuousSpeed = MAX(0, m_continuousSpeed - 1);
+            m_driveSpeed = MAX(0, m_driveSpeed - 1);
         }
     }
     else
     {
-        m_currentMenu = MIN(3, m_currentMenu + 1);
+        m_currentMenu = MIN(5, m_currentMenu + 1);
+        m_settingsMode = 0;
     }
 
     updateDisplay();
 }
 
+/**
+ * Signal that the SELECT key has been pressed.
+ */
 void Menu::keySelect()
 {
-    m_isEditing = !m_isEditing;
+    if (!m_isEditing)
+    {
+        m_isEditing = true;
+    }
+    else
+    {
+        if (m_currentMenu == 1)
+        {
+            if (m_settingsMode == 1)
+            {
+                loadValues();
+            }
+            else if (m_settingsMode == 2 || m_settingsMode == 3)
+            {
+                if (m_settingsMode == 3)
+                {
+                    resetValues();
+                }
+
+                saveValues();
+            }
+
+            m_settingsMode = 0;
+            m_isEditing = false;
+        }
+        else
+        {
+            m_isEditing = false;
+        }
+    }
 
     updateDisplay();
 }
